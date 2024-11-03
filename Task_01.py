8fa35e891185c3ec32e0c69f5382178882b5d736
rathnayr <rathnayr@metropolia.fi>
2024-11-03 23:16:33 +0530

    Update Task_01.py

--- Task_01.py
+++ Task_01.py
@@ -1,1 +1,35 @@
 
+class Publication:
+    def __init__(self, name):
+        self.name = name
+
+    def print_info(self):
+        print(self.name, end=" ")
+
+
+class Book(Publication):
+    def __init__(self, name, author, pages):
+        super().__init__(name)
+        self.author = author
+        self.pages = pages
+
+    def print_info(self):
+        super().print_info()
+        print("(author" + self.author + " " + str(self.pages) + "pages)")
+
+
+class Magazine(Publication):
+    def __init__(self, name, editor):
+        super().__init__(name)
+        self.editor = editor
+
+    def print_info(self):
+        super().print_info()
+        print("editor" + self.editor)
+
+
+pub = []
+pub.append(Magazine("Donald Duck", "Aki Hyppa"))
+pub.append(Book("Compartment No.6", "Rosa Likson", 192))
+for i in pub:
+    i.print_info()
